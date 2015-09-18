c6e942525b00fb0fdb3cc8092e00bd137992ff46
Fabio Vergani <fabio.vergani.1975@gmail.com>
2015-09-18 20:16:13 +0200

    Update CSSprefix_determine.js

--- CSSprefix_determine.js
+++ CSSprefix_determine.js
@@ -1,14 +1,25 @@
 //createTag=d.createElement.bind(d),
 
-var css=(function(fake){
- var p,e=fake.style,o={'O':'o','Moz':'moz','Webkit':'webkit'},x={prefix:'',vendor:null,animationEvent:{animation:{end:'animationend',start:'animationstart'}}};
- for(p in o){
-  if(p+'Animation' in e){
-	p=o[p];
-	x.vendor='-'+p+'-';
-	if(!'animation' in e){x.prefix=x.vendor;x.animationEvent={end:p+'AnimationEnd',start:p+'AnimationStart'};};
-	break;
-  };
- };
- return x;
-})(createTag('fake'));
+	var css=(function(fake){
+		var p,e=fake.style,
+		o={'O':'o','Moz':'moz','Webkit':'webkit'},
+		a='Animation',
+		s='animation',
+		n='Name',
+		t=a+n,
+		x={prefix:'',vendor:null,animationName:s+n,animationEvent:{end:s+'end',start:s+'start'}};
+		for(p in o) {
+			if(p+a in e){
+				n=o[p];
+				x.vendor='-'+n+'-';
+				if(!s in e){
+					x.prefix=o;
+					x.animationName=p+t;
+					a=n+a;
+					x.animationEvent={end:a+'End',start:a+'Start'};
+				};
+				break;
+			};
+		};
+		return x;
+	})(createTag('fake')),
